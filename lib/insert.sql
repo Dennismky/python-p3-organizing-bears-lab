df8044ebef59baefd48e96192b65e5a13c685391
Dennis Kyalo <kyalodennis1738@gmail.com>
2025-05-23 13:31:53 +0300

    final

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(1, "Mr.Chocolate", 40, "M", "White", "melancolic", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(2, "Rowdy", 20, "M", "Black", "choleric", 0);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(3, "Tabitha", 30, "F", "White", "grinchy", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(4, "Sergent", 40, "M", "Brown", "aggressive", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(5, "Melissa", 10, "F", "Black", "melancolic", 0);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(6, "Grinch", 50, "M", "White", "naive", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(7, "Wendy", 27, "F", "Brown", "choleric", 1);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(8, null, 32, "M", "White", "melancolic", 0);
